2ffd7d00f9d31e9b09525c5b4ed3b72c297dbb73
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-05-10 11:29:40 +0200

    styling in login/sigup

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,14 +1,17 @@
 import {Link} from "react-router-dom";
-
+import '../css/signup.css'
 const SignIn = () => {
     return (
-        <div className="signIn">
-            <form className='signIn-form'>
-                <h1 className='signIn-form__h1'>Log In</h1>
-                <input className='signIn-form__email' type="email" placeholder='Enter email'/>
-                <input className='signIn-form__password' type="password" placeholder='Password'/>
+        <div className="wrapper">
+            <form className='wrapper-form'>
+                <h1 className='wrapper-form__h1'>Sign In</h1>
+                <label htmlFor="signin-email">Email</label>
+                <input id='signin-email' className='wrapper-form__email' type="email" placeholder='Enter email' required/>
+                <label htmlFor="signin-password">Password</label>
+                <input id='signin-password' className='wrapper-form__password' type="password" placeholder='Password' required/>
+                <button className='wrapper-form__button'>Sign In</button>
             </form>
-            <p className='signIn__p'>Don't have an account? <Link to='/signup'>Sign up</Link></p>
+            <p className='wrapper__p'>Don't have an account yet? <Link to='/signup'>Sign Up</Link></p>
         </div>
     )
 }
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import SignIn from "./SignIn";
+import {Link} from "react-router-dom";
+import '../css/signup.css'
 
 function SignUp() {
     return(
-        <div className="signup">
-
+        <div className="wrapper">
+            <form className='wrapper-form'>
+                <h1 className='wrapper-form__h1'>Sign Up</h1>
+                <label htmlFor="signup-email">Email</label>
+                <input id='signup-email' className='wrapper-form__email' type="email" placeholder='Enter email' required/>
+                <label htmlFor="signup-password">Password</label>
+                <input id='signup-password' className='wrapper-form__password' type="password" placeholder='Password' required/>
+                <button className='wrapper-form__button'>Sign Up</button>
+            </form>
+            <p className='wrapper__p'>Already have an account? <Link to='/'>Sign In</Link></p>
         </div>
     )
 }
